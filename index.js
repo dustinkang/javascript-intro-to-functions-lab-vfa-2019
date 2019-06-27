f02d9dff86d357f4a208dd1aa06b64d90a8c97ab
Dustin Kang <dustinkang13@gmail.com>
2019-06-27 04:04:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log()
+  console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
